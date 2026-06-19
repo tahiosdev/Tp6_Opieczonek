a242cd2126276f3e94f30ee0a4bbc2a0ee3b93fa
enzooooo <tahiel.opieczonek@gmail.com>
2026-06-19 16:02:18 -0300

    Ej 6 / mitad

---
+++ Ej_6/src/ej_6/Ej_6.java
@@ -0,0 +1,12 @@
+package ej_6;
+
+public class Ej_6 {
+
+    public static void main(String[] args) {
+        
+        Libro librito_1 = new Libro("Los tres chanchitos", "Anonimo?", 10, 0);
+        Libro librito_2 = new Libro();
+        
+    }
+    
+}
---
+++ Ej_6/src/ej_6/Libro.java
@@ -0,0 +1,74 @@
+package ej_6;
+
+public class Libro {
+    
+    private String titulo;
+    private String autor;
+    private int ejemplares;
+    private int prestados;
+    
+    //-----CONSTRUCTORES-----
+    public Libro() {
+    }
+    
+    public Libro(String titulo, String autor, int ejemplares, int prestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ejemplares = ejemplares;
+        this.prestados = prestados;
+    }
+    
+    //-----METODOS-----
+    void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    void setEjemplares(int ejemplares) {
+        this.ejemplares = ejemplares;
+    }
+    
+    void setPrestador(int prestados) {
+        this.prestados = prestados;
+    }
+    
+    String getTitulo() {
+        return this.titulo;
+    }
+    
+    String getAutor() {
+        return this.autor;
+    }
+    
+    int getEjemplares() {
+        return this.ejemplares;
+    }
+    
+    int getPrestados() {
+        return this.prestados;
+    }
+    
+    boolean prestamo() {
+        if (this.ejemplares == 0) {
+            return false;
+        } else {
+            this.ejemplares--;
+            this.prestados++;
+            return true;
+        }
+    }
+    
+    boolean devolucion() {
+        if (this.prestados == 0) {
+            return false;
+        } else {
+            this.ejemplares++;
+            this.prestados--;
+            return true;
+        }
+    }
+    
+}
